3d4f38b409a12d1a122752d3c5be5497a94fbb31
Ilya Lyasin <ilya.lyasin07@mail.ru>
2024-10-23 21:22:29 +0300

    add args parser

--- dirbust3r.py
+++ dirbust3r.py
@@ -1,11 +1,24 @@
+from xmlrpc.client import boolean
+
 from rich.console import Console
 from rich.text import Text
-from rich.panel import Panel
+import argparse
 
 
 
 def main():
-    ...
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--url', type=str, help='url to scan', required=True)
+    parser.add_argument('-w', '--wordlist', type=str, help='wordlist to use', required=True)
+    parser.add_argument('-r', '--robots', action='store_true', help='include robots.txt file', default=False)
+    parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity', default=False)
+    args = parser.parse_args()
+
+    hello()
+
+    if args.robots:
+        ...
+
 
 
 def import_from_dict():
@@ -26,7 +39,9 @@ def hello():
     console.print("made by eliassen", style="bold red")
 
 
+def robots():
+    ...
 
 if __name__ == '__main__':
     console = Console()
-    hello()
+    main()
